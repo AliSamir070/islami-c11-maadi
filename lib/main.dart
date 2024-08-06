10e62fe6b88643e37c7373742985187eeb59855d
Dell <marina.magdy.ibrahim97@gmail.com>
2024-08-06 21:48:29 +0300

    +add shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islamy_c11_maadi/style/AppStyle.dart';
+import 'package:islamy_c11_maadi/style/configuration.dart';
 import 'package:islamy_c11_maadi/ui/SettingsProvider.dart';
 import 'package:islamy_c11_maadi/ui/hadeth_content.dart';
 import 'package:islamy_c11_maadi/ui/home/home_screen.dart';
@@ -7,19 +8,39 @@ import 'package:islamy_c11_maadi/ui/suracontent/sura_content_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   // new change
-
-
+  await SavedPreferences.init();
   runApp(ChangeNotifierProvider(
-      create: (context)=>SettingsProvider(),
-      child: const MyApp())  );
+      create: (context) => SettingsProvider()..getProviderMode()..getProviderLanguage(), child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  SavedPreferences savedPreferences = SavedPreferences();
+  ThemeMode appMode = ThemeMode.light;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    savedPreferences.getLanguage();
+  }
+
+  getMode() async {
+    String mode = await savedPreferences.getModeString();
+    appMode = savedPreferences.getThemeMode(mode);
+  }
+
   @override
   Widget build(BuildContext context) {
     SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
@@ -27,26 +48,22 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       darkTheme: AppStyle.darkTheme,
-      themeMode: settingsProvider.themeMode,
+      themeMode: appMode,
       theme: AppStyle.lightTheme,
-      localizationsDelegates: [
+      localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        Locale("en"),
-        Locale("ar")
-      ],
+      supportedLocales: const [Locale("en"), Locale("ar")],
       locale: Locale(settingsProvider.selectedLanguage),
       routes: {
-        HomeScreen.routename:(_)=>HomeScreen(),
-        SuraContentScreen.routeName:(_)=>SuraContentScreen(),
-        HadethContentScreen.routname:(_)=>HadethContentScreen()
+        HomeScreen.routename: (_) =>  HomeScreen(),
+        SuraContentScreen.routeName: (_) => SuraContentScreen(),
+        HadethContentScreen.routname: (_) => const HadethContentScreen()
       },
       initialRoute: HomeScreen.routename,
     );
   }
 }
-
--- lib/ui/SettingsProvider.dart
+++ lib/ui/SettingsProvider.dart
@@ -2,9 +2,17 @@
 // publisher
 
 import 'package:flutter/material.dart';
+import 'package:islamy_c11_maadi/style/configuration.dart';
 
 class SettingsProvider extends ChangeNotifier{
   ThemeMode themeMode = ThemeMode.light;
+  SavedPreferences savedPreferences=SavedPreferences();
+
+  getProviderMode(){
+    String theme=savedPreferences.getModeString();
+    themeMode=savedPreferences.getThemeMode(theme);
+    notifyListeners();
+  }
 
   ChangeThemeMode(ThemeMode newTheme){
     if(newTheme == themeMode) return;
@@ -18,6 +26,10 @@ class SettingsProvider extends ChangeNotifier{
     }else{
       return "assets/images/background.png";
     }
+  }
+  getProviderLanguage(){
+    selectedLanguage=savedPreferences.getLanguage();
+    notifyListeners();
   }
 
   String selectedLanguage = "en";
